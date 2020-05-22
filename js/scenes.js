d1a508873bc89cc714904d051fdb887559320028
sjhoffiz <sj.hoffiz@gmail.com>
2020-05-22 19:36:03 -0400

    all text are centered in all non-level game screens

--- js/scenes.js
+++ js/scenes.js
@@ -11,31 +11,31 @@ var mode = TITLE_SCREEN;
 function gameOverScreen() {
 	colorRect(0, 0, c.width, c.height, 'blue');
 	colorText("Game Over", c.width/2 - 80, c.height/2, "30px arial", "white");
-	colorText("New Game: [SPACE]", 330, 350, "15px arial", "white");
+	colorText("New Game: [SPACE]", c.width/2 - 70, c.height/2 + 40, "15px arial", "white");
 }
 
 function winScreen() {
 	colorRect(0, 0, c.width, c.height, 'green');
 	colorText("Player Wins", c.width/2 - 80, c.height/2, "30px arial", "white");
-	colorText("New Game: [SPACE]", 330, 350, "15px arial", "white");
+	colorText("New Game: [SPACE]", c.width/2 - 70, c.height/2 + 40, "15px arial", "white");
 }
 
 function titleScreen() {
 	colorRect(0, 0, c.width, c.height, 'red');
 	colorText("Title Screen", c.width/2 - 80, c.height/2, "30px arial", "white");
-	colorText("New Game: [SPACE]", 330, 350, "15px arial", "white");
+	colorText("New Game: [SPACE]", c.width/2 - 70, c.height/2 + 40, "15px arial", "white");
 }
 
 function mainMenuScreen() {
 	colorRect(0, 0, c.width, c.height, 'orange');
 	colorText("Main Menu", c.width/2 - 80, c.height/2, "30px arial", "white");
-	colorText("New Game: [ENTER]", 330, 350, "15px arial", "white");
-	colorText("Fullscreen: [F]", 330, 400, "15px arial", "white");
-	colorText("Credits: [SHIFT]", 330, 450, "15px arial", "white");
+	colorText("New Game: [ENTER]", c.width/2 - 70, c.height/2 + 40, "15px arial", "white");
+	colorText("Fullscreen: [F]", c.width/2 - 70, c.height/2 + 80, "15px arial", "white");
+	colorText("Credits: [SHIFT]", c.width/2 - 70, c.height/2 + 120, "15px arial", "white");
 }
 
 function creditScreen() {
 	colorRect(0, 0, c.width, c.height, 'black');
 	colorText("Credits", c.width/2 - 80, c.height/2, "30px arial", "white");
-	colorText("Main Menu: [SPACE]", 330, 350, "15px arial", "white");
+	colorText("Main Menu: [SPACE]", c.width/2 - 70, c.height/2 + 40, "15px arial", "white");
 }
