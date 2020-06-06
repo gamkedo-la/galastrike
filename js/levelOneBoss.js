c5388259286cd738e576e16209c9de006a5d6ecd
Simon <sj.hoffiz@gmail.com>
2020-06-06 19:13:39 -0400

    Boss > shot class WIP

--- js/levelOneBoss.js
+++ js/levelOneBoss.js
@@ -15,20 +15,13 @@ function levelOneBossClass() {
 	this.fullyOnScreen = false;
 	this.hpToChangeToFullyOnScreen = 90;
 
-	this.dropLoot = false;
-	this.lootX;
-	this.lootY;
-	this.lootW = 30;
-	this.lootH = 30;
-	this.lootRate = 1; // = 1/1 of the time loot drops when enemy dies
-	this.lootYDrift = 1; // speed at which loot drifts to bottom of screen
-
 	this.shotX;
 	this.shotY;
 	this.shotW = 5;
 	this.shotH = 10;
 	this.shotActive = false;
 	this.shotSpeed = 9;
+	this.myShot = [];
 
 
 	this.draw = function () {
@@ -37,17 +30,16 @@ function levelOneBossClass() {
 		colorText(this.hp, this.x + this.w, this.y + this.h - 150, "18px arial", "orange"); // hp indicator
 
 		if (this.shotActive == true) {
-			colorRect(this.shotX, this.shotY, this.shotW, this.shotH, 'white');
+			//colorRect(this.shotX, this.shotY, this.shotW, this.shotH, 'white');
 		}
-		this.basicShot();
-		
 
-		if (this.dropLoot == true) {
-			colorRect(this.lootX, this.lootY, this.lootW, this.lootH, 'green');
+		for (var i = 0; i < this.myShot.length; i++) {
+			this.myShot[i].draw();
 		}
 	}
 
 	this.move = function () {
+		this.basicShot();
 		//movement ai
 
 		if (this.enteredScreen == false) {
@@ -69,18 +61,23 @@ function levelOneBossClass() {
 			}
 		}
 	
-
-		if (this.dropLoot == true) {
-			this.lootY += this.lootYDrift;
-		}
-
 		this.collitionDetection();
-		this.lootPickUp();
 
 		if (this.shotActive == true) {
 			this.shotY += this.shotSpeed;
 			this.shotCheck();
 		}
+
+		for (var i = 0; i < this.myShot.length; i++) {
+			this.myShot[i].move();
+		}
+	}
+
+	this.fireShot = function () {
+		var newShot = new levelOneBossShotClass();
+		newShot.shotActive = true;
+		this.myShot.push(newShot);
+		this.reloadFrames = newShot.shotReloadRate;
 	}
 
 	this.shotHitMeCheck = function (theShot) {
@@ -94,14 +91,15 @@ function levelOneBossClass() {
 	}
 
 	this.basicShot = function () {
-		if (this.shotActive == false) {
-			this.rn = Math.round(Math.random() * (3 - 1) + 1);
-			if (this.rn == 2) { // fiering from middle orb
-				this.shotActive = true;
-				this.shotY = this.y + this.h - 100;
-				this.shotX = this.x + this.w / 2;
-				//playBossShootingSound();
-			}
+			this.rn = Math.round(Math.random() * (60 - 1) + 1); 
+			console.log(this.rn);
+			if (this.rn == 1) { // fiering from middle orb
+				console.log("FIRE!!!!");
+				//this.shotY = this.y + this.h - 100;
+				//this.shotX = this.x + this.w / 2;
+				this.fireShot();
+				
+			/*
 			if (this.rn == 3) { //fiering from right orb
 				this.shotActive = true;
 				this.shotY = this.y + this.h - 40;
@@ -113,13 +111,10 @@ function levelOneBossClass() {
 				this.shotY = this.y + this.h - 80;
 				this.shotX = this.x + this.w / 2 - 180;
 				//playBossShootingSound();
-			}
-		}
-
-		if (this.shotY >= c.height) {
-			this.shotActive = false;
+			} */
 		}
 	}
+	
 
 	this.shotCheck = function () {
 		if (p1.collisionCheck(false, this.shotX, this.shotY, this.shotW, this.shotH)) {
@@ -133,23 +128,6 @@ function levelOneBossClass() {
 			p1.getHit();
 			console.log("add player collision bounce to player");
 		
-		}
-	}
-
-	this.lootDrop = function () {
-		this.rn = Math.round(Math.random() * ((this.lootRate) - 1) + 1);
-		console.log("a1 loot rate:" + this.rn);
-		if (this.rn == 1) {
-			this.dropLoot = true;
-			this.lootX = this.x;
-			this.lootY = this.y;
-		}
-	}
-
-	this.lootPickUp = function () {
-		if (this.lootX >= p1.x && this.lootX + this.lootW <= p1.x + PLAYER_SHIP_WIDTH && this.lootY >= p1.y && this.lootY <= p1.y + PLAYER_SHIP_HEIGHT) {
-			p1.weaponUpgrade();
-			this.dropLoot = false;
 		}
 	}
 
